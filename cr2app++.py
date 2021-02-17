dbfe80a00032042af8bee4b319eef98823bd87a3
Rodrigo Sparrapan <sparodrigo82+343dfjdfgsflk45634576-463456ldkjh3456.4523457=?345werdfasdsfgdfhdfjdfgsfldkjhlkdfgsd@gmail.com>
2021-02-17 00:54:02 -0800

    ...

---
+++ cr2app++.py
@@ -0,0 +1,60 @@
+import rawpy
+import imageio
+import glob, os
+import cv2
+import glob as glob
+
+"""path = "ToMinistery/drive/*.CR2" #rawfile #o seu arquivo direto da camera.
+fotos = glob.glob(path)
+
+for file in fotos:
+    raw = rawpy.imread(file)
+    rgb = raw.postprocess(use_auto_wb=True)
+    imageio.imsave(file+'.jpg', rgb)
+
+
+
+count = 1
+
+path = "ToMinistery/drive/*.jpg"
+filenames = glob.glob(path)
+
+for filename in filenames:
+        
+        image = cv2.imread(filename)
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        #image = cv2.resize(image, (6000, 40000))
+        #image = cv2.imread(image)
+        #gray_img = cv2.resize(gray_img, (6000, 4000))
+    
+        cv2.imwrite("ToMinistery/drive/gray/ministery_gray_" +str(count) +".jpg", gray_img)
+        cv2.imwrite("ToMinistery/drive/color/ministery_color_" +str(count) +".jpg", image)
+    
+        count += 1
+"""
+
+
+
+count = 1
+
+path = "ToMinistery/drive/*CR2.jpg" 
+path = "ToMinistery/drive/*.jpg" 
+path = "ToMinistery/drive/*.JPG" 
+filenames = glob.glob(path)
+
+for filename in filenames:
+        
+        image = cv2.imread(filename)
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        #image = cv2.resize(image, (6000, 40000))
+        #image = cv2.imread(image)
+        #gray_img = cv2.resize(gray_img, (6000, 4000))
+    
+        cv2.imwrite("ToMinistery/drive/_gray_" +str(count) +".jpg", gray_img)
+        cv2.imwrite("ToMinistery/drive/_color_" +str(count) +".jpg", image)
+    
+        count += 1
+
+#maybe same folder #ou voce pode mudar as pastas se quiser.
