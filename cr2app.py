7849a7151d4cfbafee4e96744cf06778fa9b5af8
Rodrigo Sparrapan <sparodrigo82+343dfjdfgsflk45634576-463456ldkjh3456.4523457=?345werdfasdsfgdfhdfjdfgsfldkjhlkdfgsd@gmail.com>
2021-02-16 21:14:07 -0800

    Update cr2app.py

--- cr2app.py
+++ cr2app.py
@@ -2,11 +2,11 @@ import rawpy
 import imageio
 import glob, os
 
-path = "F/*.CR2"
+path = "F/*.CR2" #rawfile #o seu arquivo direto da camera.
 fotos = glob.glob(path)
 
 for file in fotos:
     raw = rawpy.imread(file)
     rgb = raw.postprocess(use_auto_wb=True)
-    imageio.imsave(file+'.png', rgb)
+    imageio.imsave(file+'.png', rgb) #maybe same folder #ou voce pode mudar as pastas se quiser.
 
